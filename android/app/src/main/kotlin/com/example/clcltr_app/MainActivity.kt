b165266ff3af8c5e2fe721cee97aceb51056d18a
Andrey Tsaryk <tsaryk1323an@gmail.com>
2021-06-12 20:51:26 +0300

    one part project new calculator

---
+++ android/app/src/main/kotlin/com/example/clcltr_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.clcltr_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Calculator",
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  __HomePageState createState() => __HomePageState();
+}
+
+class __HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    Widget custombutton(String btnval) {
+      return Expanded(
+        child: OutlineButton(
+          padding: EdgeInsets.all(25.0),
+          onPressed: () {},
+          child: Text(
+            "$btnval",
+            style: TextStyle(
+              fontSize: 25.0,
+            ),
+          ),
+        ),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Calculator",
+        ),
+        centerTitle: true,
+      ),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(10.0),
+                alignment: Alignment.bottomRight,
+                child: Text(
+                  "Output",
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                custombutton("9"),
+                custombutton("8"),
+                custombutton("7"),
+                custombutton("+"),
+              ],
+            ),
+            Row(
+              children: [
+                custombutton("6"),
+                custombutton("5"),
+                custombutton("4"),
+                custombutton("-"),
+              ],
+            ),
+            Row(
+              children: [
+                custombutton("3"),
+                custombutton("2"),
+                custombutton("1"),
+                custombutton("*"),
+              ],
+            ),
+            Row(
+              children: [
+                custombutton("C"),
+                custombutton("0"),
+                custombutton("="),
+                custombutton("/"),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
